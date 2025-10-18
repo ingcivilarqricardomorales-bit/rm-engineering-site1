70805f07aaa47ce1c549758d725007e17628b6a3
ingcivilarqricardomorales-bit <ing.civil.arq.ricardo.morales@gmail.com>
2025-10-18 01:41:11 -0500

    Delete src directory

--- src/components/CaseStudy.jsx
+++
@@ -1,9 +0,0 @@
-export default function CaseStudy({ title, intro, body }) {
-  return (
-    <article className="prose prose-neutral max-w-none">
-      <h1>{title}</h1>
-      <p className="lead">{intro}</p>
-      {body?.map((b,i)=>(<p key={i}>{b}</p>))}
-    </article>
-  )
-}
--- src/components/ProjectGrid.jsx
+++
@@ -1,9 +0,0 @@
-import ProjectCard from './ProjectCard'
-
-export default function ProjectGrid({ projects }) {
-  return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {projects.map(p => <ProjectCard key={p.id} project={p} />)}
-    </div>
-  )
-}
